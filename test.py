2e49771e04a05d7ea4df5aa755c5baf29e2f8418
HaiDreamer <nghuyhai2004@gmail.com>
2026-01-26 21:46:55 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("hojjatk/mnist-dataset")
+
+print("Path to dataset files:", path)
+
+# Path to dataset files: C:\Users\Admin\.cache\kagglehub\datasets\hojjatk\mnist-dataset\versions\1
